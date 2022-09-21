585cff57a1e19987257d14c52324f865896b8669
FerdikDesp <ferdikdesp@mail.ru>
2022-09-21 23:12:43 +0300

    Добавлен лог действий

--- src/ru/vsu/cs/tableForm.java
+++ src/ru/vsu/cs/tableForm.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
+import java.util.Date;
 
 
 public class tableForm extends JFrame {
@@ -28,6 +29,16 @@ public class tableForm extends JFrame {
 
     private Table table;
     private boolean wasTableGenerated = false;
+    Date date;
+
+    private void addToLog(String log) {
+        date = new Date();
+        if (Logs.getText().isEmpty()) {
+            Logs.setText("[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "] " + log);
+        } else {
+            Logs.setText(Logs.getText() + "\n[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "] " + log);
+        }
+    }
 
     public tableForm() {
         this.setTitle("Таблица");
@@ -50,11 +61,14 @@ public class tableForm extends JFrame {
         ConcoleTable.setEnabled(false);
         ConcoleTable.setBackground(Color.black);
 
+        addToLog("Программа успешно запущена!");
+
         buttonIncreaseColumns.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 tableModel.setColumnCount(tableModel.getColumnCount() + 1);
                 table.addColumn();
+                addToLog("Количество столбцов увеличено до " + tableModel.getColumnCount());
             }
         });
 
@@ -64,8 +78,10 @@ public class tableForm extends JFrame {
                 if (tableModel.getColumnCount() > 0) {
                     tableModel.setColumnCount(tableModel.getColumnCount() - 1);
                     table.removeColumn();
+                    addToLog("Количество столбцов уменьшено до " + tableModel.getColumnCount());
                 } else {
                     JOptionPane.showMessageDialog(tableForm.this, "Вы не можете удалить несуществующий столбец!", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                    addToLog("Не удалось уменьшить количество столбцов");
                 }
             }
         });
@@ -75,6 +91,7 @@ public class tableForm extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 tableModel.setRowCount(tableModel.getRowCount() + 1);
                 table.addRow();
+                addToLog("Количество строк увеличено до " + tableModel.getRowCount());
             }
         });
 
@@ -84,8 +101,10 @@ public class tableForm extends JFrame {
                 if (tableModel.getRowCount() > 0) {
                     tableModel.setRowCount(tableModel.getRowCount() - 1);
                     table.removeRow();
+                    addToLog("Количество строк уменьшено до " + tableModel.getRowCount());
                 } else {
                     JOptionPane.showMessageDialog(tableForm.this, "Вы не можете удалить несуществующую строку!", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                    addToLog("Не удалось уменьшить количество строк");
                 }
             }
         });
@@ -100,6 +119,7 @@ public class tableForm extends JFrame {
                 tableMain.setModel(tableModel);
                 tableMain.getTableHeader().setResizingAllowed(false);
                 tableMain.getTableHeader().setReorderingAllowed(false);
+                addToLog("Инициализирована новая таблица");
             }
         });
 
@@ -115,6 +135,7 @@ public class tableForm extends JFrame {
                     }
                     ConcoleTable.setText(ConcoleTable.getText() + "\n");
                 }
+                addToLog("Таблица была заполнена числами");
             }
         });
 
@@ -129,6 +150,7 @@ public class tableForm extends JFrame {
                     }
                     ConcoleTable.setText(ConcoleTable.getText() + "\n");
                 }
+                addToLog("Значения ячеек на окне обновлены в классе таблицы");
             }
         });
     }
