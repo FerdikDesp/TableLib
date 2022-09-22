fa3356636a1f0914e4e82dca95c9fe8c148c1665
FerdikDesp <ferdikdesp@mail.ru>
2022-09-22 22:04:06 +0300

    Удаление классов Column и Row, реализация класса Cell, использование класса в классе таблицы

--- src/ru/vsu/cs/Table.java
+++ src/ru/vsu/cs/Table.java
@@ -4,12 +4,12 @@ import java.util.Random;
 
 public class Table {
 
-    private String[][] dataTable;
+    private Cell[][] dataTable;
     private int columnCount;
     private int rowCount;
 
     public Table(int rowCount, int columnCount) {
-        dataTable = new String[rowCount][columnCount];
+        dataTable = new Cell[rowCount][columnCount];
         this.columnCount = columnCount;
         this.rowCount = rowCount;
     }
@@ -18,21 +18,21 @@ public class Table {
         Random random = new Random();
         for (int i = 0; i < dataTable.length; i++) {
             for (int j = 0; j < dataTable[i].length; j++) {
-                dataTable[i][j] = Integer.toString(random.nextInt(201) - 100);
+                dataTable[i][j].setValue(Integer.toString(random.nextInt(201) - 100));
             }
         }
     }
 
     public String getCellValue(int row, int column) {
-        return dataTable[row][column];
+        return dataTable[row][column].getValue();
     }
 
     public void setCellValue(int row, int column, String value) {
-        dataTable[row][column] = value;
+        dataTable[row][column].setValue(value);
     }
 
     public void addRow() {
-        String[][] tempTable = new String[dataTable.length + 1][dataTable[0].length];
+        Cell[][] tempTable = new Cell[dataTable.length + 1][dataTable[0].length];
         for (int i = 0; i < dataTable.length; i++) {
             for (int j = 0; j < dataTable[i].length; j++) {
                 tempTable[i][j] = dataTable[i][j];
@@ -42,7 +42,7 @@ public class Table {
     }
 
     public void addColumn() {
-        String[][] tempTable = new String[dataTable.length][dataTable[0].length + 1];
+        Cell[][] tempTable = new Cell[dataTable.length][dataTable[0].length + 1];
         for (int i = 0; i < dataTable.length; i++) {
             for (int j = 0; j < dataTable[i].length; j++) {
                 tempTable[i][j] = dataTable[i][j];
@@ -53,7 +53,7 @@ public class Table {
 
     public void removeRow() {
         if (dataTable.length >= 1) {
-            String[][] tempTable = new String[dataTable.length - 1][dataTable[0].length];
+            Cell[][] tempTable = new Cell[dataTable.length - 1][dataTable[0].length];
             for (int i = 0; i < dataTable.length - 1; i++) {
                 for (int j = 0; j < dataTable[i].length; j++) {
                     tempTable[i][j] = dataTable[i][j];
@@ -65,7 +65,7 @@ public class Table {
 
     public void removeColumn() {
         if (dataTable[0].length >= 1) {
-            String[][] tempTable = new String[dataTable.length][dataTable[0].length - 1];
+            Cell[][] tempTable = new Cell[dataTable.length][dataTable[0].length - 1];
             for (int i = 0; i < dataTable.length; i++) {
                 for (int j = 0; j < dataTable[i].length - 1; j++) {
                     tempTable[i][j] = dataTable[i][j];
